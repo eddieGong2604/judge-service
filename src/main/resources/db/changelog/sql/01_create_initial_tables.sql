e36f97ca5a9724754f35deefd75bd9f92f30b8c7
phuvinhbmt <phuvinhbmt@gmail.com>
2021-04-28 23:15:21 +1000

    rebase vinh

--- src/main/resources/db/changelog/sql/01_create_initial_tables.sql
+++ src/main/resources/db/changelog/sql/01_create_initial_tables.sql
@@ -2,10 +2,20 @@ SET search_path to test_service_schema;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp" with schema public;
 
-CREATE TABLE IF NOT EXISTS ATTEMPT (
-    id uid;
-    createdOn TIMESTAMP;
-    isDeleted boolean;
-   	code varchar;
-    PRIMARY KEY(id)
+create table if not exists attempt(
+    id uuid PRIMARY KEY,
+    created_on timestamp default CURRENT_TIMESTAMP,
+    is_deleted BOOL default 'f',
+    code varchar
+);
+create table if not exists test_case(
+    id uuid PRIMARY KEY,
+    created_on timestamp default CURRENT_TIMESTAMP,
+    is_deleted BOOL default 'f',
+    attempt_id uuid,
+    stdout varchar,
+    verdict varchar,
+    expected_output varchar,
+    input varchar,
+    constraint fk_qualification_practitioner foreign key(attempt_id) references attempt(id)
 );
