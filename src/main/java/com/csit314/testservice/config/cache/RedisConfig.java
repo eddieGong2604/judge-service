f7f637bb9a1fc6a5aa457647248af203f4301e79
loc le <tienloc.le2604@gmail.com>
2021-04-29 02:25:06 +0700

    update env

--- src/main/java/com/csit314/testservice/config/cache/RedisConfig.java
+++ src/main/java/com/csit314/testservice/config/cache/RedisConfig.java
@@ -3,17 +3,52 @@ package com.csit314.testservice.config.cache;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
 @Configuration
 public class RedisConfig {
 
     @Bean
-    public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory connectionFactory) {
+    JedisPoolConfig jedisPoolConfig() {
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxTotal(10);
+        poolConfig.setMaxIdle(5);
+        poolConfig.setMinIdle(1);
+        poolConfig.setTestOnBorrow(true);
+        poolConfig.setTestOnReturn(true);
+        poolConfig.setTestWhileIdle(true);
+        return poolConfig;
+    }
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory(JedisPoolConfig jedisPoolConfig) throws  URISyntaxException {
+        String envRedisUrl = System.getenv("REDIS_URL");
+        URI redisUri = new URI(envRedisUrl);
+        RedisStandaloneConfiguration hostConfig = new RedisStandaloneConfiguration();
+        hostConfig.setPort(redisUri.getPort());
+        hostConfig.setHostName(redisUri.getHost());
+        hostConfig.setPassword(redisUri.getUserInfo().split(":", 2)[1]);
+        JedisClientConfiguration.JedisClientConfigurationBuilder builder = JedisClientConfiguration.builder();
+        JedisClientConfiguration clientConfig = builder
+                .usePooling()
+                .poolConfig(jedisPoolConfig)
+                .build();
+
+        return new JedisConnectionFactory(hostConfig, clientConfig);
+
+    }
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate(JedisConnectionFactory jedisConnectionFactory) {
         final RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(connectionFactory);
+        redisTemplate.setConnectionFactory(jedisConnectionFactory);
         redisTemplate.setEnableTransactionSupport(true);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
