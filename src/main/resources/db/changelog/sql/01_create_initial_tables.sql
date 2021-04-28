c079488fb8f0c1afd22c0c2f369db27725f1b05c
phuvinhbmt <phuvinhbmt@gmail.com>
2021-04-28 23:15:21 +1000

    add attempt table

--- src/main/resources/db/changelog/sql/01_create_initial_tables.sql
+++ src/main/resources/db/changelog/sql/01_create_initial_tables.sql
@@ -1,3 +1,11 @@
 SET search_path to test_service_schema;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp" with schema public;
+
+CREATE TABLE IF NOT EXISTS ATTEMPT (
+    id uid;
+    createdOn TIMESTAMP;
+    isDeleted boolean;
+   	code varchar;
+    PRIMARY KEY(id)
+);
