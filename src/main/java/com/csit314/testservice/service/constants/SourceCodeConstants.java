27ad02b26377c0ec1cf82be8db45143b5755dba9
loc le <tienloc.le2604@gmail.com>
2021-05-09 21:53:06 +0700

    add hardcode input

---
+++ src/main/java/com/csit314/testservice/service/constants/SourceCodeConstants.java
@@ -0,0 +1,206 @@
+package com.csit314.testservice.service.constants;
+
+public class SourceCodeConstants {
+    public static final String CORRECT_SOURCE_CODE = "#include <iostream>\n" +
+            "#include <fstream>\n" +
+            "#include <string>\n" +
+            "#include <climits>\n" +
+            "#include <math.h>\n" +
+            "#include <cfloat>\n" +
+            "\n" +
+            "using namespace std;\n" +
+            "char fileName[50];\n" +
+            "int nVertices, nEdges;\n" +
+            "\n" +
+            "struct Vertex {\n" +
+            "    double x;\n" +
+            "    double y;\n" +
+            "};\n" +
+            "\n" +
+            "struct Dist {\n" +
+            "    int vertex;\n" +
+            "    double distance;\n" +
+            "};\n" +
+            "\n" +
+            "\n" +
+            "Vertex *vertices;\n" +
+            "double **edges;\n" +
+            "int start, goal;\n" +
+            "\n" +
+            "double findShortestPath (Vertex *vertices, double **edges, int nVertices, int start, int goal, int *path);\n" +
+            "void siftup(Dist *heap, int pos);\n" +
+            "void swap(Dist *heap, int pos1, int pos2);\n" +
+            "void makeheap(Dist *heap, int end);\n" +
+            "void siftdown(Dist *heap, int end, int pos);\n" +
+            "double heuristic(int vertex, int goal);\n" +
+            "double findSecondShortestPath (Vertex *vertices, double **edges, int nVertices, int start, int goal, int *shortestPath, int *secondShortestPath);\n" +
+            "\n" +
+            "int main () {\n" +
+            "    \n" +
+            "    cin>>nVertices>>nEdges;\n" +
+            "    vertices = new Vertex[nVertices+1];\n" +
+            "    edges = new double*[nVertices+1];\n" +
+            "    int *path = new int[nVertices+1];\n" +
+            "    int *path2nd = new int[nVertices+1];\n" +
+            "    for (int i = 0; i <= nVertices ; i++) {\n" +
+            "        edges[i] = new double[nVertices+1];\n" +
+            "        for (int j = 0; j <= nVertices ; j++) {\n" +
+            "            edges[i][j] = INT_MAX;\n" +
+            "        }\n" +
+            "    }\n" +
+            "    int label;\n" +
+            "    double x, y;\n" +
+            "    for (int i = 0; i< nVertices; i++) {\n" +
+            "         Vertex vertex;\n" +
+            "        cin >> label >> x >> y;\n" +
+            "        vertices[label].x = x;\n" +
+            "        vertices[label].y = y;\n" +
+            "    }\n" +
+            "\n" +
+            "    int edge1, edge2;\n" +
+            "    double weight;\n" +
+            "    for (int i = 0; i < nEdges ; i++) {\n" +
+            "            cin >> edge1 >> edge2 >> weight;\n" +
+            "            if (edges[edge1][edge2] == DBL_MAX || edges[edge1][edge2] > weight) {\n" +
+            "                edges[edge1][edge2] = weight;\n" +
+            "                edges[edge2][edge1] = weight;\n" +
+            "            }\n" +
+            "        \n" +
+            "    }\n" +
+            "    cin >> start >> goal;\n" +
+            "\n" +
+            "    int length;\n" +
+            "    double shortestPath = findShortestPath (vertices, edges, nVertices, start, goal, path);\n" +
+            "\n" +
+            "    cout <<\"Shortest path from \"<< start <<\" to \" << goal <<\": \" << endl;\n" +
+            "    cout << \"Vertices of path: \";\n" +
+            "    int v = start;\n" +
+            "    while (v!=goal) {\n" +
+            "        cout << v <<\"-->\";\n" +
+            "        v = path[v];\n" +
+            "    };\n" +
+            "    cout << goal << endl;\n" +
+            "    cout << \"Length of path: \" << shortestPath << endl;\n" +
+            "\n" +
+            "    double secondShortestPath = findSecondShortestPath (vertices, edges, nVertices, start, goal, path, path2nd);\n" +
+            "    cout <<\"\\nSecond shortest path from \"<< start <<\" to \" << goal <<\": \" << endl;\n" +
+            "    cout<<\"Vertices of path: \";\n" +
+            "    v = start;\n" +
+            "    while (v!=goal) {\n" +
+            "        cout << v <<\"-->\";\n" +
+            "        v = path2nd[v];\n" +
+            "    };\n" +
+            "    cout << goal << endl;\n" +
+            "    cout << \"Length of path: \"<< secondShortestPath << endl;\n" +
+            "\n" +
+            "    return 0;\n" +
+            "}\n" +
+            "\n" +
+            "double findShortestPath (Vertex *vertices, double **edges, int nVertices, int start, int goal, int *path) {\n" +
+            "    Dist *distance = new Dist[nVertices]; \n" +
+            "    int endDistance = 0;\n" +
+            "    double size[nVertices+1]; \n" +
+            "    for (int i = 1; i <=nVertices; i++ ) {\n" +
+            "        if (i != goal) {\n" +
+            "            if (edges[goal][i]!=DBL_MAX ) {\n" +
+            "                path[i] = goal;\n" +
+            "            }\n" +
+            "            size[i] = edges[goal][i];\n" +
+            "            distance[endDistance].vertex = i;\n" +
+            "            distance[endDistance].distance = edges[goal][i];\n" +
+            "            endDistance++;\n" +
+            "            siftup(distance, endDistance-1);\n" +
+            "        }\n" +
+            "        \n" +
+            "    }\n" +
+            "    \n" +
+            "    do {\n" +
+            "        Dist v = distance[0]; \n" +
+            "        if (v.vertex == start) {\n" +
+            "            return size[start];\n" +
+            "        }\n" +
+            "\n" +
+            "        endDistance--;\n" +
+            "        distance[0] = distance[endDistance];\n" +
+            "        for (int i = 0; i < endDistance ; i++) {\n" +
+            "            Dist u = distance[i];\n" +
+            "            if ( edges[v.vertex][u.vertex] != INT_MAX  && u.distance > v.distance + edges[v.vertex][u.vertex] ) {\n" +
+            "                distance[i].distance = v.distance + edges[v.vertex][u.vertex];\n" +
+            "                path[u.vertex] = v.vertex;\n" +
+            "                size[u.vertex] = v.distance + edges[v.vertex][u.vertex]; //update the total path\n" +
+            "            }\n" +
+            "        }\n" +
+            "        makeheap(distance, endDistance);\n" +
+            "    }\n" +
+            "    while (endDistance > 0);\n" +
+            "\n" +
+            "    return size[start];\n" +
+            "};\n" +
+            "\n" +
+            "\n" +
+            "double findSecondShortestPath (Vertex *vertices, double **edges, int nVertices, int start, int goal, int *shortestPath, int *secondShortestPath) {\n" +
+            "    int v = start;\n" +
+            "    double secondShortestPathSize = DBL_MAX;\n" +
+            "    while (v!=goal) {\n" +
+            "        int *path = new int[nVertices+1];\n" +
+            "        int next = shortestPath[v];\n" +
+            "        double removedEdge = edges[v][next];\n" +
+            "        edges[v][next] = DBL_MAX;\n" +
+            "        edges[next][v] = DBL_MAX;\n" +
+            "        double pathSize = findShortestPath(vertices, edges, nVertices, start, goal, path); //find shortest path not counting edge e[i]\n" +
+            "        edges[v][next] = removedEdge;\n" +
+            "        edges[next][v] = removedEdge;\n" +
+            "        if (pathSize < secondShortestPathSize) {\n" +
+            "            secondShortestPathSize = pathSize;\n" +
+            "            for (int i = 0; i<= nVertices; i++) {\n" +
+            "                secondShortestPath[i] = path[i];\n" +
+            "            }\n" +
+            "        };\n" +
+            "        v = next;\n" +
+            "    }\n" +
+            "\n" +
+            "    return secondShortestPathSize;\n" +
+            "}\n" +
+            "\n" +
+            "void makeheap(Dist *heap, int end) {\n" +
+            "    for(int i=end/2; i>=0;i--) {\n" +
+            "        siftdown(heap, end, i);\n" +
+            "    };\n" +
+            "};\n" +
+            "\n" +
+            "void siftdown(Dist *heap, int end, int pos) {\n" +
+            "    int child = pos*2+1; //since my heap start from 0 => child = pos*2+1 and pos*2+2\n" +
+            "    if(pos==end-1|| child>end-1) //if pos reached end of array=> return \n" +
+            "        return;\n" +
+            "    if(child+1<end && heap[child].distance + heuristic(heap[child].vertex, start) > heap[child+1].distance + heuristic(heap[child+1].vertex, start)) {\n" +
+            "        child++;\n" +
+            "    };\n" +
+            "    if(heap[pos].distance + heuristic(heap[pos].vertex, start) > heap[child].distance + heuristic(heap[child].vertex, start)) {\n" +
+            "        swap(heap, pos,child);\n" +
+            "        siftdown(heap,end,child);\n" +
+            "    };\n" +
+            "};\n" +
+            "\n" +
+            "void siftup(Dist *heap, int pos) {\n" +
+            "    if(pos==0) {\n" +
+            "        return;\n" +
+            "    }\n" +
+            "    int parent=(pos-1)/2;\n" +
+            "    if (heap[parent].distance + heuristic(heap[parent].vertex, start) <heap[pos].distance + heuristic(heap[pos].vertex, start)) {\n" +
+            "        return;\n" +
+            "    } else {\n" +
+            "        swap(heap, parent, pos);\n" +
+            "        siftup(heap, parent);\n" +
+            "    }\n" +
+            "}\n" +
+            "\n" +
+            "void swap(Dist *heap, int pos1, int pos2) {\n" +
+            "    Dist temp=heap[pos1];\n" +
+            "    heap[pos1]=heap[pos2];\n" +
+            "    heap[pos2]=temp;\n" +
+            "};\n" +
+            "\n" +
+            "double heuristic(int vertex, int start) {\n" +
+            "    return sqrt(pow(vertices[vertex].x-vertices[start].x, 2)+pow(vertices[vertex].y-vertices[start].y,2));\n" +
+            "}\n";
+}
--- src/main/java/com/csit314/testservice/service/impl/TestCaseGenerationServiceImpl.java
+++ src/main/java/com/csit314/testservice/service/impl/TestCaseGenerationServiceImpl.java
@@ -7,6 +7,7 @@ import com.csit314.testservice.integration.judge0.dto.request.SubmissionBatchReq
 import com.csit314.testservice.integration.judge0.dto.request.SubmissionRequestDto;
 import com.csit314.testservice.integration.judge0.dto.response.SubmissionBatchResponseDto;
 import com.csit314.testservice.service.TestCaseGenerationService;
+import com.csit314.testservice.service.constants.SourceCodeConstants;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
@@ -24,14 +25,12 @@ public class TestCaseGenerationServiceImpl implements TestCaseGenerationService 
     private final Judge0ServiceIntegration judge0ServiceIntegration;
     private final String ASSIGNMENT_CACHE_KEY = "CSCI203_ASSIGNMENT3";
     private final RedisTemplate<String, CachedAssignment> assignmentCache;
-
     @PostConstruct
     public void init() {
         if (assignmentCache.hasKey(ASSIGNMENT_CACHE_KEY)) {
             assignmentCache.delete(ASSIGNMENT_CACHE_KEY);
         }
     }
-
     @Override
     public List<TestCaseResponseDto> generateTestCase() throws InterruptedException {
         final ValueOperations<String, CachedAssignment> operations = assignmentCache.opsForValue();
@@ -41,33 +40,141 @@ public class TestCaseGenerationServiceImpl implements TestCaseGenerationService 
         if (assignmentCache.hasKey(ASSIGNMENT_CACHE_KEY) && (operations.get(ASSIGNMENT_CACHE_KEY)).getCachedTestCases() != null) {
             return testCaseMapper.fromCachedToResponseTestCase(Objects.requireNonNull(operations.get(ASSIGNMENT_CACHE_KEY)).getCachedTestCases());
         }
-
         SubmissionBatchRequestDto submissionBatchRequestDto = new SubmissionBatchRequestDto();
-        /*Generate 10 test cases*/
-        for (int i = 0; i < 10; i++) {
-            submissionBatchRequestDto.getSubmissions().add(SubmissionRequestDto.builder().source_code(
-                    /*TODO: This is the CORRECT source code that we used to generate test cases. Currently it is just a
-                       correct source code for a simple problem. The task to do is
-                       - Store the correct source code for assignment 3 to Google Cloud Storage
-                       - Get the source code from Google Cloud Storage and replace the below string with that source code*/
-                    "#include <iostream> \n using namespace std; \n int main(){int a;cin >> a;cout<< a;return 0;}"
-            )
-                    .stdin(
-                            /*TODO: generate input for Assignment 3 in the inputGenerator() function, currently it just return random value*/
-                            String.valueOf(inputGenerator())
+        /*Generate 1 medium test cases*/
+        submissionBatchRequestDto.getSubmissions().add(SubmissionRequestDto.builder().source_code(SourceCodeConstants.CORRECT_SOURCE_CODE).stdin(inputGenerator()).language_id(52).build());
 
-                    ).language_id(52).build());
-        }
         SubmissionBatchResponseDto submissionBatchResponseDto = judge0ServiceIntegration.getExpectedOutputBatch(submissionBatchRequestDto);
-        CachedAssignment updatedCachedAssignment = CachedAssignment.builder().cachedTestCases(testCaseMapper.fromSubmissionBatchResponseToCachedTestCase(submissionBatchResponseDto)).assignmentName(ASSIGNMENT_CACHE_KEY).code("#include <iostream> \n using namespace std; \n int main(){int a;cin >> a;cout<< a;return 0;}").build();
+        CachedAssignment updatedCachedAssignment = CachedAssignment.builder().cachedTestCases(testCaseMapper.fromSubmissionBatchResponseToCachedTestCase(submissionBatchResponseDto)).assignmentName(ASSIGNMENT_CACHE_KEY).code(SourceCodeConstants.CORRECT_SOURCE_CODE).build();
 
         assignmentCache.delete(ASSIGNMENT_CACHE_KEY);
         operations.set(ASSIGNMENT_CACHE_KEY, updatedCachedAssignment);
         return testCaseMapper.fromSubmissionBatchResponseToTestcaseResponseDto(submissionBatchResponseDto);
     }
-
-    private int inputGenerator() {
-        Random random = new Random();
-        return random.nextInt(10);
+    /*TODO: Gen 5 medium Input, 4 small inputs, and 5 big input, 1 edge case*/
+    private String inputGenerator() {
+        return "20\t100\t\n" +
+                "1\t6\t25\n" +
+                "2\t12\t66\n" +
+                "3\t16\t23\n" +
+                "4\t21\t24\n" +
+                "5\t24\t39\n" +
+                "6\t32\t27\n" +
+                "7\t36\t98\n" +
+                "8\t40\t67\n" +
+                "9\t52\t53\n" +
+                "10\t54\t4\n" +
+                "11\t76\t16\n" +
+                "12\t81\t64\n" +
+                "13\t83\t98\n" +
+                "14\t85\t96\n" +
+                "15\t88\t81\n" +
+                "16\t91\t73\n" +
+                "17\t94\t78\n" +
+                "18\t95\t29\n" +
+                "19\t98\t80\n" +
+                "20\t99\t84\n" +
+                "1\t3\t19\n" +
+                "1\t6\t34\n" +
+                "1\t9\t58\n" +
+                "1\t12\t94\n" +
+                "1\t15\t108\n" +
+                "1\t16\t104\n" +
+                "2\t1\t46\n" +
+                "2\t4\t47\n" +
+                "2\t5\t36\n" +
+                "2\t10\t82\n" +
+                "2\t13\t85\n" +
+                "2\t16\t88\n" +
+                "2\t17\t91\n" +
+                "2\t18\t96\n" +
+                "3\t8\t57\n" +
+                "3\t10\t49\n" +
+                "3\t12\t84\n" +
+                "3\t13\t106\n" +
+                "4\t10\t44\n" +
+                "4\t12\t82\n" +
+                "4\t18\t80\n" +
+                "5\t4\t22\n" +
+                "5\t17\t88\n" +
+                "5\t18\t79\n" +
+                "5\t20\t93\n" +
+                "6\t2\t53\n" +
+                "6\t4\t19\n" +
+                "6\t19\t93\n" +
+                "7\t8\t36\n" +
+                "7\t10\t102\n" +
+                "7\t15\t64\n" +
+                "7\t16\t65\n" +
+                "8\t1\t60\n" +
+                "8\t4\t56\n" +
+                "8\t6\t46\n" +
+                "8\t9\t26\n" +
+                "8\t12\t47\n" +
+                "8\t19\t64\n" +
+                "9\t3\t56\n" +
+                "9\t4\t48\n" +
+                "9\t8\t28\n" +
+                "9\t16\t50\n" +
+                "10\t3\t51\n" +
+                "10\t9\t58\n" +
+                "10\t13\t108\n" +
+                "11\t3\t68\n" +
+                "11\t7\t96\n" +
+                "11\t9\t51\n" +
+                "11\t10\t34\n" +
+                "11\t12\t55\n" +
+                "11\t19\t77\n" +
+                "11\t20\t77\n" +
+                "12\t2\t76\n" +
+                "12\t3\t84\n" +
+                "12\t5\t69\n" +
+                "12\t7\t64\n" +
+                "12\t10\t73\n" +
+                "12\t11\t58\n" +
+                "12\t17\t25\n" +
+                "12\t18\t44\n" +
+                "12\t20\t36\n" +
+                "13\t6\t93\n" +
+                "13\t9\t63\n" +
+                "14\t8\t61\n" +
+                "14\t9\t64\n" +
+                "14\t12\t40\n" +
+                "14\t16\t28\n" +
+                "14\t17\t29\n" +
+                "15\t4\t95\n" +
+                "15\t5\t84\n" +
+                "15\t11\t71\n" +
+                "15\t17\t16\n" +
+                "16\t2\t85\n" +
+                "16\t4\t90\n" +
+                "16\t7\t67\n" +
+                "16\t8\t57\n" +
+                "16\t10\t84\n" +
+                "16\t15\t17\n" +
+                "17\t9\t58\n" +
+                "17\t14\t28\n" +
+                "17\t15\t15\n" +
+                "17\t19\t11\n" +
+                "17\t20\t12\n" +
+                "18\t5\t78\n" +
+                "18\t10\t57\n" +
+                "18\t12\t42\n" +
+                "18\t14\t72\n" +
+                "18\t19\t61\n" +
+                "18\t20\t63\n" +
+                "19\t3\t104\n" +
+                "19\t4\t105\n" +
+                "19\t5\t90\n" +
+                "19\t15\t16\n" +
+                "19\t17\t10\n" +
+                "20\t1\t119\n" +
+                "20\t6\t94\n" +
+                "20\t9\t64\n" +
+                "20\t12\t36\n" +
+                "20\t13\t31\n" +
+                "20\t14\t28\n" +
+                "2\t13\n";
     }
+
 }
